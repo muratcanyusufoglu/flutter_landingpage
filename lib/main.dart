7e30d782dee263c22188aeee1de6c60619e19edd
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-02-02 16:53:17 +0300

    Container List

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,74 @@ class MainClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Sofia'),
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: SafeArea(
-          child: Column(
-            //mainAxisSize: MainAxisSize.min, //ekranda kapladığı yer
-            crossAxisAlignment:
-                CrossAxisAlignment.stretch, //row hizalama center, end, stretch
-            mainAxisAlignment:
-                MainAxisAlignment.spaceEvenly, //spacebetween, center vs.
-            children: <Widget>[
-              Container(
-                  child: Text('asd'),
-                  height: 100,
-                  width: 200,
-                  color: Colors.lime),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      child: Text('asd'),
-                      height: 100,
-                      width: 100,
-                      color: Colors.black54,
-                    ),
-                    Container(
-                      child: Text('asd'),
-                      height: 100,
-                      width: 100,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min, //ekranda kapladığı yer
+              //crossAxisAlignment:CrossAxisAlignment.center, //row hizalama center, end, stretch
+              //mainAxisAlignment:MainAxisAlignment.spaceEvenly, //spacebetween, center vs.
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundColor: Colors.lime,
+                  backgroundImage: AssetImage('assets/images/forma.jpeg'),
+                ),
+                Text('TS CLUB',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white70,
+                        fontFamily: 'Sofia')),
+                Text('OFFICALL APP THE TRABZONSPOR CLUB',
+                    style: TextStyle(
+                      fontSize: 30,
                       color: Colors.white70,
+                    )),
+                Container(
+                  margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black87,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.mail,
+                          size: 80,
+                          color: Colors.white70,
+                        ),
+                        Text('trabzonspor@gmail.com',
+                            style:
+                                TextStyle(fontSize: 40, color: Colors.white70)),
+                      ],
                     ),
-                  ]),
-              Container(
-                child: Text('asd'),
-                height: 100,
-                width: 200,
-                color: Colors.cyan,
-              ),
-            ],
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black87,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(children: <Widget>[
+                        Icon(
+                          Icons.telegram,
+                          size: 80,
+                          color: Colors.white70,
+                        ),
+                        Text('0561 610 61 61',
+                            style:
+                                TextStyle(fontSize: 40, color: Colors.white70)),
+                      ]),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
